ac42c3fd2cdda700b053cccfaebb8fcbdf6363be
66945 <dtmcgra@gmail.com>
2023-01-11 10:40:04 -0700

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by McGrath, Daniel - Student on 1/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
