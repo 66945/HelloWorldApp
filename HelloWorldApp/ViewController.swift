6301eb7cd3af8853ae1f49754b70f30ebefbef34
66945 <dtmcgra@gmail.com>
2023-01-11 11:12:12 -0700

    done

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var favoriteBand: UILabel?
+    var sentance = UILabel()
+    let members = 2
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        favoriteBand?.text = "Daft Punk"
+        
+        sentance.frame = CGRect(x: 0, y: 0, width: 200, height: 21)
+        sentance.center = CGPoint(x: 100, y: 500)
+        sentance.text = "Daft Punk has \(members) members"
+        self.view.addSubview(sentance)
     }
-
-
 }
 
